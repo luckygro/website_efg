2077cf0cde4ef32f59e51a35059c9891c5b27de9
luckygro <lukas@luckyg.de>
2019-02-13 23:37:55 +0100

    add firebase and travis

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,4 +1,17 @@
-/*
+exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
+  if (stage === "build-html") {
+    actions.setWebpackConfig({
+      module: {
+        rules: [
+          {
+            test: /leaflet-src/,
+            use: loaders.null(),
+          },
+        ],
+      },
+    })
+  }
+}/*
 
 const Promise = require('bluebird')
 const path = require('path')
