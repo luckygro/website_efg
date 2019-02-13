b0c3d3a89a29ab7847818c1b7a47348331a66a52
luckygro <lukas@luckyg.de>
2019-02-13 15:02:55 +0100

    animation regular events

--- src/components/regular.js
+++ src/components/regular.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import Remarkable from 'remarkable'
 
+import '../style/regular.css'
+
 // render
 
 var md = new Remarkable({
@@ -8,39 +10,67 @@ var md = new Remarkable({
   breaks: true,
 })
 
-function EventTile(props) {
+class EventTile extends Component {
+  state = {
+    status: 'closed',
+    style: {maxHeight: '0px'},
+  }
 
-  const {
-    title,
-    image,
-    bibleText,
-    bibleVerse,
-    dateDay,
-    dateTime,
-    description,
-    descriptionhtml = md.render(description.description),
-    isImage = image 
-  } = props.node
+  handleChange = () => {
 
-  return (
-    <div style={{
-      width: "50%",
-      backgroundColor: "#fff",
-      marginBottom: "20px"
-      }}>
+    // update state
+    if(this.state.status == 'open') {
+      this.setState({
+        status: 'close',
+        style: {maxHeight: '0px'}
+      })
+    
+    } else {
+      this.setState({
+        status: 'open',
+        style: {maxHeight: '300px'}
+      })
+    }
 
-      <div style={{backgroundColor: "#ddd", padding: "10px 20px"}}>
-        <h4 style={{margin: "0", padding: "0", color: "#fff"}}>{title}</h4>
-        <span style={{color: "#fff"}}>{dateDay}, {dateTime} Uhr</span>
+    // update style
+
+  }
+
+  render() {
+
+    const {
+      title,
+      image,
+      bibleText,
+      bibleVerse,
+      dateDay,
+      dateTime,
+      description,
+      descriptionhtml = md.render(description.description),
+      isImage = image 
+    } = this.props.content
+  
+    return (
+      <div className="EventTile" onClick={this.handleChange}>
+
+        <div className="TileHeader">
+          <h4 style={{margin: "0", padding: "0", color: "#fff"}}>{title}</h4>
+          <span style={{color: "#fff"}}>{dateDay}, {dateTime} Uhr</span>
+        </div>
+
+        <div className="TileContent" style={this.state.style}>
+          <div dangerouslySetInnerHTML={{ __html: descriptionhtml }} />
+          <div>
+            <span style={{color: "#29ABE2"}}>{bibleText}</span>
+            <span style={{color: "#999999"}}> - {bibleVerse}</span>
+          </div>
+        </div>
+        
       </div>
-      <div style={{padding: "10px 20px"}}>
-        <span dangerouslySetInnerHTML={{ __html: descriptionhtml }} />
-        <span style={{color: "#29ABE2"}}>{bibleText}</span>
-        <span style={{color: "#999"}}> - {bibleVerse}</span>
-      </div>
-      
-    </div>
-  )
+    )
+
+  }
+  
 }
 
 const RegularEvents = ({ list }) => (
@@ -58,11 +88,11 @@ class Regular extends Component {
     return (
         <section style={{backgroundColor: "#eee"}}>
           <div className="Container">
-            <h3>Regelmäßige Veranstaltungen</h3>
+            <h3>Veranstaltungen</h3>
             <div>
               {regularEvents.map(({ node }) => {
                   return (
-                    <EventTile node={node} key={node.slug} />
+                    <EventTile content={node} key={node.slug} />
                   )
                 })}
             </div>
