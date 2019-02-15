c64e03130240069816887540240674464b965abf
luckygro <lukas@luckyg.de>
2019-02-15 23:16:17 +0100

    responsive menu

--- src/components/header.js
+++ src/components/header.js
@@ -6,6 +6,30 @@ import logo from '../images/efg_logo.png'
 import '../style/header.css';
 
 class Header extends Component {
+
+  state = {
+    status: 'close',
+    style: ''
+  }
+
+  handleChange = () => {
+
+    // update state
+    if(this.state.status == 'open') {
+      this.setState({
+        status: 'close',
+        style: {maxHeight: '0px'}
+      })
+    
+    } else {
+      this.setState({
+        status: 'open',
+        style: {maxHeight: '300px'}
+      })
+    }
+    console.log(this.state.status)
+  } 
+ 
   render() {
     return (
         <header>
@@ -13,7 +37,8 @@ class Header extends Component {
             <div className="LogoImage" >
               <img alt="Logo der evangelisch freien Gemeinde in Pforzheim" src={logo} ></img>
             </div>
-            <nav>
+            <button className="Nav" onClick={this.handleChange}>MENÜ</button>
+            <nav className={this.state.status}>
               <div>
                 <a href="/">Home</a>
                 <a href="/glaubensbekenntnis">Glaubensbekenntnis</a>
